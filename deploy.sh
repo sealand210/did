449d5180f56b877532a9c4ae770558e865eb4981
Evan Hung <mail2evanhung@gmail.com>
2012-03-23 18:34:25 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
-git remote set-url origin git@github.com:sealand210/sealand210.github.com.git
-git config branch.remote origin
+@echo Deploying github pages...
+git checkout master
 rake generate
+cd ~/blog_DID/public/
+git remote add origin git@github.com:sealand210/sealand210.github.com.git
 git add .
-git commit -am'update'
-rake deploy
+git commit -am 'update'
+cd ~/blog_DID/
 git remote set-url origin git@github.com:sealand210/did.git
+git add .
+git commit -am 'update'
 git push origin master
 git config branch.remote heroku
 git push heroku master
