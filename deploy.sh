cb21d4a5cc15749f66c7e9a84680db6623b8873d
Evan Hung <mail2evanhung@gmail.com>
2012-03-23 19:17:31 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 echo Deploying github pages...
 rake generate
-cd ~/blog_DID/public/
-git init
-git remote add origin git@github.com:sealand210/sealand210.github.com.git
+rm -rf _deploy/*
+cp -rf public/ _deploy/
+cd _deploy/
 git add .
 git commit -am 'update'
 git push -f origin master
 echo Backing up github pages...
-cd ~/blog_DID/
-git remote set-url origin git@github.com:sealand210/did.git
+cd ../
 git add .
 git commit -am 'update'
 git push origin master
 echo Deploying heroku pages...
 git config branch.remote heroku
-git remote set-url origin git@heroku.com:d-i-d.git
 git push heroku master
 
 
